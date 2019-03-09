b63e0607732fe82e9a350e933fbcfbea7036010a
PogoL <patryk.pogorzelczyk@gmail.com>
2019-03-09 19:10:05 +0100

    Database-First Workflow Excercises

---
+++ Vidzy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+    public enum Classification : byte
+    {
+        Silver = 1,
+        Gold,
+        Platinum
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VidzyDbContext context = new VidzyDbContext();
+
+            context.AddVideo("film_47", DateTime.Now, "Horror", (byte)Classification.Platinum);
+        }
+    }
+}

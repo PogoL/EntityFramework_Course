930e9098c2d4d2296cc150bd970ab29c2a00b9cf
PogoL <patryk.pogorzelczyk@gmail.com>
2019-03-10 11:10:49 +0100

    Exercise #1 - Action movies sorted by name

--- VidzyExercise6/Vidzy/Program.cs
+++ VidzyExercise6/Vidzy/Program.cs
@@ -1,11 +1,22 @@
 ﻿
 
+using System.Linq;
+
 namespace Vidzy
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new VidzyContext();
+
+            var query1 = context.Videos.Where(v => v.Genre.Name.Contains("Action")).OrderBy(v => v.Name);
+            foreach (var video in query1)
+            {
+                System.Console.WriteLine(video.Name);
+            }
+
+            System.Console.ReadKey();
         }
     }
 }

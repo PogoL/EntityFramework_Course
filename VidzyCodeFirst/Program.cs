65d321e1eed3028466a6fee41c6259adb5e86375
PogoL <patryk.pogorzelczyk@gmail.com>
2019-03-10 09:16:40 +0100

    Overriding Code-First Conventions - Excercises

--- VidzyCodeFirst/Program.cs
+++ VidzyCodeFirst/Program.cs
@@ -13,13 +13,23 @@ namespace VidzyCodeFirst
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public Genre Genre { get; set; }
+        public int GenreId { get; set; }
         public Classification Classification { get; set; }
+        public List<Tag> Tags { get; set; }
     }
 
     public class Genre
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Video> Videos { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Video> Videos { get; set; }
     }
 
     public enum Classification : byte
@@ -33,7 +43,38 @@ namespace VidzyCodeFirst
     {
         public DbSet<Video> Videos { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Video>()
+                        .Property(v => v.Name)
+                        .HasMaxLength(255)
+                        .IsRequired();
+
+            modelBuilder.Entity<Video>()
+                        .HasRequired(v => v.Genre)
+                        .WithMany(g => g.Videos)
+                        .HasForeignKey(v => v.GenreId);
+
+            modelBuilder.Entity<Genre>()
+                        .Property(g => g.Name)
+                        .HasMaxLength(255)
+                        .IsRequired();
+
+            modelBuilder.Entity<Video>()
+                        .HasMany(v => v.Tags)
+                        .WithMany(t => t.Videos)
+                        .Map(m =>
+                        {
+                            m.MapLeftKey("VideoId");
+                            m.MapRightKey("TagId");
+                            m.ToTable("VideoTags");
+                        });
+                        
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 
     class Program

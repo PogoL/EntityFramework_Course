89be6126c3acb8a3a3cce92d97b3b26d9ff784f2
PogoL <patryk.pogorzelczyk@gmail.com>
2019-03-10 09:40:58 +0100

    Querying Data using LINQ - Quick Demo of LINQ syntax and Extension Methods

---
+++ Queries/Queries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            // Linq syntax
+            var query = from course in context.Courses
+                        where course.Name.Contains("c#")
+                        orderby course.Name
+                        select course;
+
+            foreach (var course in query)
+            {
+                System.Console.WriteLine(course.Name);
+            }
+
+            // Extension methods
+            var courses = context.Courses
+                                 .Where(c => c.Name.Contains("c#"))
+                                 .OrderBy(c => c.Name);
+
+            foreach (var course in courses)
+            {
+                System.Console.WriteLine(course.Name);
+            }
+        }
+    }
+}

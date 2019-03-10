fdd930c6adc8889b3d6ab347f84f7e9a395e079f
PogoL <patryk.pogorzelczyk@gmail.com>
2019-03-10 11:16:12 +0100

    Exercise 2

--- VidzyExercise6/Vidzy/Program.cs
+++ VidzyExercise6/Vidzy/Program.cs
@@ -9,12 +9,21 @@ namespace Vidzy
         static void Main(string[] args)
         {
             var context = new VidzyContext();
-
+            System.Console.WriteLine("Exercise 1: Action movies sorted by name");
             var query1 = context.Videos.Where(v => v.Genre.Name.Contains("Action")).OrderBy(v => v.Name);
             foreach (var video in query1)
             {
                 System.Console.WriteLine(video.Name);
             }
+            System.Console.WriteLine("\nExercise 2: Gold drama movies sorted by release date (newest first)");
+            var query2 = context.Videos
+                .Where(v => v.Classification == Classification.Gold && v.Genre.Name.Contains("Drama"))
+                .OrderBy(v => v.ReleaseDate);
+            foreach (var video in query2)
+            {
+                System.Console.WriteLine(video.Name);
+            }
+
 
             System.Console.ReadKey();
         }
